21a6535c5e6139ac380adecafad4e88e17f026fb
@root#master <107992028+Dior7978@users.noreply.github.com>
2024-03-12 16:08:59 +0300

    Add files via upload

---
+++ WEEK 2..ASSIGNMENT 1.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Integer data type
+  int age = 25;
+
+  // Double data type
+  double temperature = 23.5;
+
+  // String data type
+  String name = 'John Doe';
+
+  // List data type (dynamic list of integers)
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // Map data type (key-value pairs)
+  Map<String, dynamic> person = {
+    'name': 'Alice',
+    'age': 30,
+    'isEmployed': true,
+  };
+
+  // printing the values
+  print('Age: $age'); // Output: Age: 25
+  print('Temperature: $temperature'); // Output: Temperature: 23.5
+  print('Name: $name'); // Output: Name: John Doe
+  print('Numbers: $numbers'); // Output: Numbers: [1, 2, 3, 4, 5]
+  print('person: $person'); // Output: person: {name: Alice, age: 30, isEmployed: true}
+}
+
